be651592b6803c346d5bba7962fdff07215d7494
Bobi <bobi.bg@gmail.com>
2022-07-27 18:12:18 +0300

    Java basics

--- src/Homework/Exercises8/SummerOutfit.java
+++ src/Homework/Exercises8/SummerOutfit.java
@@ -1,7 +1,27 @@
 package Homework.Exercises8;
 
+import java.util.Scanner;
+
 public class SummerOutfit {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int deg = Integer.parseInt(scan.nextLine());
+        String time = scan.nextLine();
+
+        //10 <= deg <= 18
+
+        if (deg >= 10 && deg <= 18 ){
+        switch (time){
+            case "Мorning":
+                System.out.printf("It's %.2d degrees, get your Sweatshirt and Sneakers. ", deg );
+                break;
+            case "Afternoon":
+                System.out.printf("It's %.2d degrees, get your Shirt and Moccasins. ", deg );
+                break;
+
+        }
 
     }
 }
