43db24e4a1a297ec09a454b078e139297a6df3fb
Bobi <bobi.bg@gmail.com>
2022-07-27 17:12:23 +0300

    Java basics

--- src/Homework/Cinema.java
+++ src/Homework/Cinema.java
@@ -1,4 +1,30 @@
 package Homework;
 
+import java.util.Scanner;
+
 public class Cinema {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String type = scan.nextLine();
+        int rows = Integer.parseInt(scan.nextLine());
+        int columns = Integer.parseInt(scan.nextLine());
+
+        int all = rows * columns;
+        double income = 0.0;
+
+        switch (type){
+            case "Premiere":
+                income = all * 12.00;
+                break;
+            case "Normal":
+                income = all * 7.50;
+                break;
+            case "Discount":
+                income = all * 5.00;
+                break;
+        }
+        System.out.printf("%.2f leva", income);
+
+    }
 }
