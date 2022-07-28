31ae2d9b7eaaab0ff6ca1df4293782c2672abca1
Bobi <bobi.bg@gmail.com>
2022-07-28 21:30:33 +0300

    Java basics

--- src/Homework/Exercises8/SummerOutfit.java
+++ src/Homework/Exercises8/SummerOutfit.java
@@ -10,36 +10,19 @@ public class SummerOutfit {
         int deg = Integer.parseInt(scan.nextLine());
         String time = scan.nextLine();
 
-        //10 <= deg <= 18
-
-        if (deg >= 10 && deg <= 18) {
-            switch (time) {
-                case "Мorning":
-                    System.out.printf("It's %.2d degrees, get your Sweatshirt and Sneakers. ", deg);
-                    break;
-                case "Afternoon":
-                    System.out.printf("It's %.2d degrees, get your Shirt and Moccasins. ", deg);
-                    break;
-
-                case "Evening":
-                    System.out.printf("It's %.2d degrees, get your Shirt and Moccasins. ", deg);
-                    break;
+        if (10 <= deg && deg <= 18){
+            switch (time){
+                case "Morning":
             }
-        } else if (18 < deg && deg <= 24) {
-            switch (time) {
-                case "Мorning":
-                    System.out.printf("It's %.2d degrees, get your YEAH and Sneakers. ", deg);
-                    break;
-                case "Afternoon":
-                    System.out.printf("It's %.2d degrees, get your Shirt and Moccasins. ", deg);
-                    break;
 
-                case "Evening":
-                    System.out.printf("It's %.2d degrees, get your Shirt and Moccasins. ", deg);
-                    break;
+        }else if (18 < deg && deg <= 24){
+
+        }else {
+
+        }
+
+
 
             }
 
         }
-    }
-}
