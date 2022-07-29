204098f0f4b7a76b92deb9e76fb116a179b78431
Bobi <bobi.bg@gmail.com>
2022-07-29 13:57:21 +0300

    Java basics

--- src/Homework/Exercises8/SummerOutfit.java
+++ src/Homework/Exercises8/SummerOutfit.java
@@ -15,20 +15,49 @@ public class SummerOutfit {
         if (10 <= deg && deg <= 18) {
             switch (time) {
                 case "Morning":
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
                     break;
                 case "Afternoon":
-                    break;
                 case "Evening":
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
                     break;
             }
 
         } else if (18 < deg && deg <= 24) {
+            switch (time) {
+                case "Morning":
+                case "Evening":
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                    break;
+                case "Afternoon":
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                    break;
+            }
 
         } else {
+            switch (time) {
+                case "Morning":
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                    break;
+                case "Evening":
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                    break;
+                case "Afternoon":
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                    break;
+
+            }
+
 
         }
-
+        System.out.printf("It's %d degrees, get your %s and %s.", deg, outfit, shoes);
 
     }
-
 }
