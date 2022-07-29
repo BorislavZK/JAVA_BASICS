425c44ce9eefde1d5f828cf72ce0e3781f609a9b
Bobi <bobi.bg@gmail.com>
2022-07-29 21:31:18 +0300

    Java basics

--- src/Homework/WorkingHours.java
+++ src/Homework/WorkingHours.java
@@ -9,52 +9,52 @@ public class WorkingHours {
         int hour = Integer.parseInt(scan.nextLine());
         String day = scan.nextLine();
 // 10 do 18 h
-        switch (day){
+        switch (day) {
             case "Monday":
-                if ( hour >= 10 && hour<= 18 ){
+                if (hour >= 10 && hour <= 18) {
                     System.out.println("open");
-                }else {
+                } else {
                     System.out.println("closed");
                 }
                 break;
 
             case "Tuesday":
-                if ( hour >= 10 && hour<= 18 ){
+                if (hour >= 10 && hour <= 18) {
                     System.out.println("open");
-                }else {
+                } else {
                     System.out.println("closed");
                 }
                 break;
             case "Wednesday":
-                if ( hour >= 10 && hour<= 18 ){
+                if (hour >= 10 && hour <= 18) {
                     System.out.println("open");
-                }else {
+                } else {
                     System.out.println("closed");
                 }
                 break;
             case "Thursday":
-                if ( hour >= 10 && hour<= 18 ){
+                if (hour >= 10 && hour <= 18) {
                     System.out.println("open");
-                }else {
+                } else {
                     System.out.println("closed");
                 }
                 break;
             case "Friday":
-                if ( hour >= 10 && hour<= 18 ){
+                if (hour >= 10 && hour <= 18) {
                     System.out.println("open");
-                }else {
+                } else {
                     System.out.println("closed");
                 }
                 break;
             case "Saturday":
-                if ( hour >= 10 && hour<= 18 ){
+                if (hour >= 10 && hour <= 18) {
                     System.out.println("open");
-                }else {
+                } else {
                     System.out.println("closed");
                 }
                 break;
             case "Sunday":
-                    System.out.println("closed");
+                System.out.println("closed");
                 break;
             default:
                 System.out.println("closed");
