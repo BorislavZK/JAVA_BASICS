9104635f72ffdd88101f295eeaead10b15d03d6d
Bobi <bobi.bg@gmail.com>
2022-08-07 16:28:30 +0300

    Java basics

---
+++ src/Homework/Exercises9/OneToN_3.java
@@ -0,0 +1,18 @@
+package Homework.Exercises9;
+
+import java.util.Scanner;
+
+public class OneToN_3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+        for (int i = n; i <= 1 ; i += 3) {
+            System.out.println(n);
+
+            }
+
+        }
+
+}
