97864fcd6b8eb321f2e4a50b6541c4466c37fd1d
Bobi <bobi.bg@gmail.com>
2022-08-07 20:27:28 +0300

    Java basics

---
+++ src/Homework/Exercises9/CharacterSequence.java
@@ -0,0 +1,4 @@
+package Homework.Exercises9;
+
+public class CharacterSequence {
+}
--- src/Homework/Exercises9/EvenPowersOf2.java
+++ src/Homework/Exercises9/EvenPowersOf2.java
@@ -1,4 +1,17 @@
 package Homework.Exercises9;
 
+import java.util.Scanner;
+
 public class EvenPowersOf2 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+        for (int i = 0; i <= n; i += 2) {
+            System.out.printf("%.0f%n",Math.pow(2,i));
+
+        }
+    }
 }
