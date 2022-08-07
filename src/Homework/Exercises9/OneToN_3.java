62d10e5187ba3a66f7f9ea0ee2bbebc42b9bca0a
Bobi <bobi.bg@gmail.com>
2022-08-07 20:08:02 +0300

    Java basics

--- src/Homework/Exercises9/OneToN_3.java
+++ src/Homework/Exercises9/OneToN_3.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class OneToN_3 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner (System.in);
+        Scanner scan = new Scanner(System.in);
 
         int n = Integer.parseInt(scan.nextLine());
 
-        for (int i = n; i <= 1 ; i += 3) {
-            System.out.println(n);
-
-            }
+        for (int i = 1; i <= n; i += 3) {
+            System.out.println(i);
 
         }
+
+    }
 
 }
