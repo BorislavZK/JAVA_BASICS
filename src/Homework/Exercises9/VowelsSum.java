1c3a87139e4ee9e684303660783a11cde7c11462
Bobi <bobi.bg@gmail.com>
2022-08-07 20:48:15 +0300

    Java basics

---
+++ src/Homework/Exercises9/VowelsSum.java
@@ -0,0 +1,12 @@
+package Homework.Exercises9;
+
+import java.util.Scanner;
+
+public class VowelsSum {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
