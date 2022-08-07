9da91e6b52ce373a2804734eea5ff448e2e5655d
Bobi <bobi.bg@gmail.com>
2022-08-07 09:01:17 +0300

    Java basics

--- src/Homework/Exercises9/N_to_1.java
+++ src/Homework/Exercises9/N_to_1.java
@@ -6,6 +6,13 @@ public class N_to_1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner (System.in);
 
+        int n = Integer.parseInt(scan.nextLine());
+
+        for (int i = n; i >= 1 ; i--) {
+            System.out.println(i);
+
+        }
+
 
     }
 }
