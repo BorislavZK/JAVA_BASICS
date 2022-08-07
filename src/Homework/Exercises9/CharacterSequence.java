9ff491fbbe6593d13ba4aa6aecf6115d8140c4d5
Bobi <bobi.bg@gmail.com>
2022-08-07 20:35:41 +0300

    Java basics

--- src/Homework/Exercises9/CharacterSequence.java
+++ src/Homework/Exercises9/CharacterSequence.java
@@ -1,4 +1,18 @@
 package Homework.Exercises9;
 
+import java.util.Scanner;
+
 public class CharacterSequence {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            char letter = input.charAt(i);
+            System.out.println(letter);
+
+        }
+    }
 }
