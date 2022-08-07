fbcbba8c029d1137908f82ff5b15432ee36c0e4d
Bobi <bobi.bg@gmail.com>
2022-08-07 21:02:17 +0300

    Java basics

--- src/Homework/Exercises9/VowelsSum.java
+++ src/Homework/Exercises9/VowelsSum.java
@@ -7,6 +7,34 @@ public class VowelsSum {
 
         Scanner scan = new Scanner(System.in);
 
+        String input = scan.nextLine();
+
+        int sum = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            switch (input.charAt(i)) {
+                case 'a':
+                    sum += 1;
+                    break;
+                case 'e':
+                    sum += 2;
+                    break;
+                case 'i':
+                    sum += 3;
+                    break;
+                case 'o':
+                    sum += 4;
+                    break;
+                case 'u':
+                    sum += 5;
+                    break;
+            }
+
+
+        }
+        System.out.println(sum);
+
+
 
     }
 }
