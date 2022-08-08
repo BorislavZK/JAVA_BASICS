ae3d4adb17d0c82e6854888007eebdc4c5e41b2b
Bobi <bobi.bg@gmail.com>
2022-08-08 13:58:27 +0300

    Java basics

---
+++ src/Homework/Exercises9/LeftAndRightSum.java
@@ -0,0 +1,14 @@
+package Homework.Exercises9;
+
+import java.util.Scanner;
+
+public class LeftAndRightSum {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+
+
+    }
+}
