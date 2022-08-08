fb9def9e7c0649a2f18def0ea5a76255cea32e6b
Bobi <bobi.bg@gmail.com>
2022-08-08 22:28:48 +0300

    Java basics

--- src/Homework/Exercises9/LeftAndRightSum.java
+++ src/Homework/Exercises9/LeftAndRightSum.java
@@ -8,7 +8,22 @@ public class LeftAndRightSum {
 
         int n = Integer.parseInt(scan.nextLine());
 
+        int sum1 = 0;
+        int sum2 = 0;
 
+        for (int i = 1; i <= n; i++) {
+            int number = Integer.parseInt(scan.nextLine());
+            sum1 += number;
+        }
+        for (int i = 1; i <= n; i++) {
+            int number = Integer.parseInt(scan.nextLine());
+            sum2 += number;
+        }
 
+        if (sum1 == sum2) {
+            System.out.printf("Yes, sum = %d", sum1);
+        } else {
+            System.out.printf("No, diff = %d", Math.abs(sum1 - sum2));
+        }
     }
 }
