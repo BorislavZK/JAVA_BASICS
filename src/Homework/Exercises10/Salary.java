a93c0f408b6aad38d346038032b22e35aa3f7ac8
Bobi <bobi.bg@gmail.com>
2022-08-16 21:46:51 +0300

    Java basics

---
+++ src/Homework/Exercises10/Salary.java
@@ -0,0 +1,36 @@
+package Homework.Exercises10;
+
+import java.util.Scanner;
+
+public class Salary {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int tabs = Integer.parseInt(scan.nextLine());
+        int salary = Integer.parseInt(scan.nextLine());
+
+        for (int i = 1; i <= tabs; i++) {
+            String currentTab = scan.nextLine();
+            switch (currentTab) {
+                case "Facebook":
+                    salary -= 150;
+                    break;
+                case "Insagram":
+                    salary -= 100;
+                    break;
+                case "Reddit":
+                    salary -= 50;
+                    break;
+            }
+
+        }
+        if (salary <= 0) {
+            System.out.println("You have lost you salary");
+
+        }
+        if (salary > 0) {
+            System.out.printf("%d", salary);
+        }
+    }
+}
+
