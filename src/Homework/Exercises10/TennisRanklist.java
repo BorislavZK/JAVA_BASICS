3230b43f66f2bd66afea39063c9ea6fcafd1eea2
Bobi <bobi.bg@gmail.com>
2022-08-17 21:54:27 +0300

    Java basics

---
+++ src/Homework/Exercises10/TennisRanklist.java
@@ -0,0 +1,10 @@
+package Homework.Exercises10;
+
+import java.util.Scanner;
+
+public class TennisRanklist {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
