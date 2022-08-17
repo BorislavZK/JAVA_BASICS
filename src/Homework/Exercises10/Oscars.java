0e3d13c513347ee65d9b31841e38480634713a5f
Bobi <bobi.bg@gmail.com>
2022-08-17 21:53:17 +0300

    Java basics

--- src/Homework/Exercises10/Oscars.java
+++ src/Homework/Exercises10/Oscars.java
@@ -10,11 +10,19 @@ public class Oscars {
         double points = Double.parseDouble(scan.nextLine());
         int n = Integer.parseInt(scan.nextLine());
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             String name = scan.nextLine();
             double juryPoints = Double.parseDouble(scan.nextLine());
             int length = name.length();
+
             points += (length * juryPoints) / 2;
+            if (points > 1250.5) {
+                System.out.println();
+                System.out.printf("Congratulations, %s got a nominee for leading role with %.1f!", nameActor, points);
+                return;
+            }
         }
+        System.out.printf("Sorry, %s you need %.1f more!", nameActor, 1250.5 - points);
     }
 }
+
