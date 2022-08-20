dcdf6878d7e16866b4164c51d43f1923045388c7
Bobi <bobi.bg@gmail.com>
2022-08-20 20:01:30 +0300

    Java basics

---
+++ src/Homework/Exercises10/TrekkingMania.java
@@ -0,0 +1,7 @@
+package Homework.Exercises10;
+
+public class TrekkingMania {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Homework/HelloSoftuni.java
+++
@@ -1,7 +0,0 @@
-package Homework;
-
-public class HelloSoftuni {
-    public static void main(String[] args) {
-System.out.println( "Hello SoftUni ");
-    }
-}
