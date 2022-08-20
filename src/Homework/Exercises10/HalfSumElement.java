ea77553e049c8520c2cf4212ab435f1a2b5bff40
Bobi <bobi.bg@gmail.com>
2022-08-20 14:29:34 +0300

    Java basics

--- src/Homework/Exercises10/HalfSumElement.java
+++ src/Homework/Exercises10/HalfSumElement.java
@@ -1,4 +1,33 @@
 package Homework.Exercises10;
 
+import java.util.Scanner;
+
 public class HalfSumElement {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int number = Integer.parseInt(scan.nextLine());
+            sum += number;
+
+            if (number > max) {
+                max = number;
+            }
+
+        }
+        int sumWithOutMaxNumber = sum - max;
+
+        if (max == sumWithOutMaxNumber) {
+            System.out.println("Yes");
+            System.out.println("Sum = " + max);
+        } else {
+            int diff = Math.abs(max - sumWithOutMaxNumber);
+            System.out.println("No");
+            System.out.println("Diff = " + diff);
+        }
+    }
 }
