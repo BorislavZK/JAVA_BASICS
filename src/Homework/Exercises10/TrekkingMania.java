eb15c5b28a0f0a27f47b942d619db4e765c25e6f
Bobi <bobi.bg@gmail.com>
2022-08-20 20:45:58 +0300

    Java basics

--- src/Homework/Exercises10/TrekkingMania.java
+++ src/Homework/Exercises10/TrekkingMania.java
@@ -1,7 +1,42 @@
 package Homework.Exercises10;
 
+import java.util.Scanner;
+
 public class TrekkingMania {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+        int musala = 0; //до 5 човека
+        int montblanc = 0; // от 6 до 12 човека
+        int kilimanjaro = 0; // от 13 до 25 човека
+        int k2 = 0; // от 26 до 40 човека
+        int everest = 0; // от 41 или повече човека
+
+        for (int i = 1; i <= n; i++) {
+            int hikers = Integer.parseInt(scan.nextLine());
+
+            if (hikers >= 1 && hikers <= 5) {
+                musala += hikers;
+            } else if (hikers >= 6 && hikers <= 12) {
+                montblanc += hikers;
+            } else if (hikers >= 13 && hikers <= 25) {
+                kilimanjaro += hikers;
+            } else if (hikers >= 26 && hikers <= 40) {
+                k2 += hikers;
+            } else {
+                everest += hikers;
+            }
+
+        }
+        int allHikers = musala + montblanc + kilimanjaro + everest;
+
+        System.out.printf("%.2f%%%n", (musala * 1.00) / allHikers * 100);
+        System.out.printf("%.2f%%%n", (montblanc * 1.00) / allHikers * 100);
+        System.out.printf("%.2f%%%n", (kilimanjaro * 1.00) / allHikers * 100);
+        System.out.printf("%.2f%%%n", (k2 * 1.00) / allHikers * 100);
+        System.out.printf("%.2f%%%n", (everest * 1.00) / allHikers * 100);
+
 
     }
 }
