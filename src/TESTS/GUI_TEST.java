2b45491d68e818b3b28000b23e76496cc69f2de6
Bobi <bobi.bg@gmail.com>
2022-08-21 10:48:30 +0300

    Java basics

---
+++ src/TESTS/GUI_TEST.java
@@ -0,0 +1,20 @@
+package TESTS;
+import javax.swing.JOptionPane;
+
+public class GUI_TEST {
+
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello "+name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You are "+age+" years old");
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null, "You are "+height+" cm tall");
+
+    }
+
+}
+
