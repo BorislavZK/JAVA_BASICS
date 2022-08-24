6238783f8c704fa81fd53836fac29c376dfda505
Bobi <bobi.bg@gmail.com>
2022-08-24 22:36:56 +0300

    Java basics

--- src/Homework/Exercises11/ReadText.java
+++ src/Homework/Exercises11/ReadText.java
@@ -5,5 +5,16 @@ import java.util.Scanner;
 public class ReadText {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+
+        while (!input.equals("stop")) {
+
+            System.out.println(input);
+            input = scan.nextLine();
+
+        }
+
+
     }
 }
