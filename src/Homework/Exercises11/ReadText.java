20a78831c338c7bd11a5a84fea4a5a47733fa840
Bobi <bobi.bg@gmail.com>
2022-08-24 22:31:37 +0300

    Java basics

---
+++ src/Homework/Exercises11/ReadText.java
@@ -0,0 +1,9 @@
+package Homework.Exercises11;
+
+import java.util.Scanner;
+
+public class ReadText {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
