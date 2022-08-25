fa7cc66b99a195a7af671ef61328d4aa31f3bb59
Bobi <bobi.bg@gmail.com>
2022-08-25 21:22:30 +0300

    Java basics

--- src/Homework/Exercises11/SumNumbers.java
+++ src/Homework/Exercises11/SumNumbers.java
@@ -1,4 +1,20 @@
 package Homework.Exercises11;
 
+import java.util.Scanner;
+
 public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+        int sum = 0;
+
+        while (sum < n){
+
+            int currentNum = Integer.parseInt(scan.nextLine());
+            sum += currentNum;
+        }
+
+        System.out.println(sum);
+    }
 }
