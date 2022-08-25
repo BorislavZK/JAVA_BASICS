0b23d3274b015259ffc55106033c81866265e0e8
Bobi <bobi.bg@gmail.com>
2022-08-25 21:36:31 +0300

    Java basics

--- src/Homework/Exercises11/AccountBalance.java
+++ src/Homework/Exercises11/AccountBalance.java
@@ -1,4 +1,9 @@
 package Homework.Exercises11;
 
+import java.util.Scanner;
+
 public class AccountBalance {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
 }
