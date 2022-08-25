5e5de84885090122439f5d9bebc68b7edb3525ff
Bobi <bobi.bg@gmail.com>
2022-08-25 21:45:29 +0300

    Java basics

--- src/Homework/Exercises11/AccountBalance.java
+++ src/Homework/Exercises11/AccountBalance.java
@@ -5,5 +5,23 @@ import java.util.Scanner;
 public class AccountBalance {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        double amount = 0;
+        String input = scan.nextLine();
+
+        while (!input.equals("NoMoreMoney")){
+
+            double currentSum = Double.parseDouble(input);
+            if ( currentSum < 0 ){
+                System.out.println("Invalid operation!");
+                break;
+            }
+            amount += currentSum;
+            System.out.printf("Increase %.2f\n", currentSum);
+
+            input = scan.nextLine();
+        }
+
+        System.out.printf("Total: %.2f", amount);
     }
 }
