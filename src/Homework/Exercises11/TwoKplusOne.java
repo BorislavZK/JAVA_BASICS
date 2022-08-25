bee724bfdb2ed94b0adf70f0aeb93077d1dbd145
Bobi <bobi.bg@gmail.com>
2022-08-25 21:35:52 +0300

    Java basics

--- src/Homework/Exercises11/TwoKplusOne.java
+++ src/Homework/Exercises11/TwoKplusOne.java
@@ -6,7 +6,13 @@ public class TwoKplusOne {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int n = Integer.parseInt(scan.nextLine());
 
+        int number = 1;
+         while ( number <= n ){
+             System.out.println(number);
+             number = number * 2 + 1;
+         }
 
     }
 }
