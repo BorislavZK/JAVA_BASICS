cabb70d570e6b266c0580109b9b8808e2a2b5a37
Bobi <bobi.bg@gmail.com>
2022-08-25 21:24:00 +0300

    Java basics

--- src/Homework/Exercises11/TwoKplusOne.java
+++ src/Homework/Exercises11/TwoKplusOne.java
@@ -1,4 +1,11 @@
 package Homework.Exercises11;
 
+import java.util.Scanner;
+
 public class TwoKplusOne {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
 }
