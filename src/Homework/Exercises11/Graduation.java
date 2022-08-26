1ebbc5c7a643c2e77c001650d761b6e0a9be26e4
Bobi <bobi.bg@gmail.com>
2022-08-26 09:48:29 +0300

    Java basics

--- src/Homework/Exercises11/Graduation.java
+++ src/Homework/Exercises11/Graduation.java
@@ -1,4 +1,38 @@
 package Homework.Exercises11;
 
+import java.util.Scanner;
+
 public class Graduation {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String name = scan.nextLine();
+        int goodGradesCount = 0;
+        int poorGradesCount = 0;
+        int gradeCount = 1;
+        double allGradeSum = 0;
+
+        while (goodGradesCount < 12) {
+
+            if (poorGradesCount == 2) {
+                break;
+            }
+            double currentGrade = Double.parseDouble(scan.nextLine());
+
+            if (currentGrade < 4) {
+                poorGradesCount++;
+                continue;
+            }
+            goodGradesCount ++;
+            gradeCount ++;
+            allGradeSum += currentGrade;
+        }
+        double avgGrade =allGradeSum / goodGradesCount;
+
+        if ( goodGradesCount >= 12){
+            System.out.printf("%s graduated. Average grade: %.2f",name, avgGrade);
+        }else {
+            System.out.printf("%s has been excluded at %d grade", name, gradeCount);
+        }
+    }
 }
---
+++ src/TESTS/whileLoopTest_1.java
@@ -0,0 +1,27 @@
+package TESTS;
+
+import java.util.Scanner;
+
+public class whileLoopTest_1 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+
+        while (!input.equals("stop")){
+
+            System.out.println(input);
+
+            input = scan.nextLine();
+
+            if (input.equals("stop")){
+                System.out.println("STOP!");
+            }
+
+
+        }
+
+
+    }
+}
