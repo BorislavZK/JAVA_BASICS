e413c5436359ff6b7449feb5056830afb078943d
Bobi <bobi.bg@gmail.com>
2022-08-26 12:05:27 +0300

    Java basics

--- src/TESTS/whileLoopTest_1.java
+++ src/TESTS/whileLoopTest_1.java
@@ -4,20 +4,15 @@ import java.util.Scanner;
 
 public class whileLoopTest_1 {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
-
-        String input = scan.nextLine();
+        String input = "";
 
         while (!input.equals("stop")){
 
-            System.out.println(input);
-
             input = scan.nextLine();
+            System.out.println(input); // принтираме СТОП като размениме местата на input и println и стринга в началото е равен на ""
 
-            if (input.equals("stop")){
-                System.out.println("STOP!");
-            }
+
 
 
         }
