5f2ef8e6fa3fdfc57b4b61f17ea0f83778706242
Bobi <bobi.bg@gmail.com>
2022-08-28 09:36:22 +0300

    Java basics

---
+++ src/Homework/Exercises12/ExamPreparation.java
@@ -0,0 +1,21 @@
+package Homework.Exercises12;
+
+import java.util.Scanner;
+
+public class ExamPreparation {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        int poorGradeCount = Integer.parseInt(scan.nextLine());
+        String input = "";
+        int poorGradeCounter = 0;
+        boolean isFalse = false;
+        int allGradesSum = 0;
+        int gradeCounter =0;
+        String lastProblem = "";
+
+
+        while (!input.equals("Enough"))
+    }
+}
