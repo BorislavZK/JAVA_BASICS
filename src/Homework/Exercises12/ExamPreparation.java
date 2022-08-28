b06421f2209d82f2c10b1fcb96ca1cc7bdb68458
Bobi <bobi.bg@gmail.com>
2022-08-28 13:04:20 +0300

    Java basics

--- src/Homework/Exercises12/ExamPreparation.java
+++ src/Homework/Exercises12/ExamPreparation.java
@@ -6,19 +6,18 @@ public class ExamPreparation {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-
         int poorGradeLimit = Integer.parseInt(scan.nextLine());
-        String input = scan.nextLine();
         int poorGradeCounter = 0;
         boolean isFalse = false;
         double allGradesSum = 0;
         int gradeCounter = 0;
         String lastProblem = "";
 
-
+        String input = scan.nextLine();
         while (!input.equals("Enough")) {
 
-            int grade = scan.nextInt();
+            int grade = Integer.parseInt(scan.nextLine());
+
             allGradesSum += grade;
             gradeCounter++;
             lastProblem = input;
@@ -26,17 +25,27 @@ public class ExamPreparation {
             if (grade <= 4) {
                 poorGradeCounter++;
             }
-            double svgScore = allGradesSum / gradeCounter;
-            if(poorGradeLimit == poorGradeCounter){
+
+            if (poorGradeLimit == poorGradeCounter) {
                 isFalse = true;
                 break;
             }
 
-             if (isFalse){
-                 // toi se e provalil
-                 System.out.printf("%d", poorGradeCounter);
+            input = scan.nextLine();
 
-             }
+
+        }
+
+        double avgScore = allGradesSum / gradeCounter;
+
+
+        if (isFalse) { // provalil
+            System.out.printf("You need a break, %d poor grades.", poorGradeCounter);
+
+
+        } else {
+            System.out.printf("Average score: %.2f\n" + "Number of problems: %.2d\n" + "Last problem: %s\n", avgScore, gradeCounter, lastProblem);
+
 
         }
     }
