97360daf141d7dc3b96489e1a0cabf9159d77921
Bobi <bobi.bg@gmail.com>
2022-08-29 16:20:04 +0300

    Java basics

--- src/Homework/Exercises12/OldBooks.java
+++ src/Homework/Exercises12/OldBooks.java
@@ -1,4 +1,20 @@
 package Homework.Exercises12;
 
+import java.util.Scanner;
+
 public class OldBooks {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        String bookName = scan.nextLine();
+        int count = 0;
+        boolean isFound = false;
+
+        String input = scan.nextLine();
+        while (!input.equals("No More Books")){
+
+
+        }
+    }
 }
