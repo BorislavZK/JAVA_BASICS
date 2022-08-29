9e596e0749c553daee7f83d86a45bdc27a424d40
Bobi <bobi.bg@gmail.com>
2022-08-29 20:59:55 +0300

    Java basics

---
+++ src/Homework/Exercises12/Walking.java
@@ -0,0 +1,34 @@
+package Homework.Exercises12;
+
+import java.util.Scanner;
+
+public class Walking {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int goal = 10000;
+        int steps = 0;
+
+
+        while (steps < goal) {
+
+            String input = scan.nextLine();
+
+            if (input.equals("Going home")){
+
+                int homeSteps = Integer.parseInt(scan.nextLine());
+                steps += homeSteps;
+                break;
+            }
+            steps += Integer.parseInt(input);
+
+        }
+
+        if (steps >= goal){
+            System.out.printf("Goal reached! Good job!\n" +
+                    "%d steps over the goal!", Math.abs(steps - goal));
+        } else {
+            System.out.printf("%d more steps to reach goal.");
+        }
+    }
+}
