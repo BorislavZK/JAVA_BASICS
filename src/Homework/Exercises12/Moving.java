67d99999200ecaee69ce5ea25933e61bc4d57fbe
Bobi <bobi.bg@gmail.com>
2022-08-30 09:30:30 +0300

    Java basics

---
+++ src/Homework/Exercises12/Moving.java
@@ -0,0 +1,18 @@
+package Homework.Exercises12;
+
+import java.util.Scanner;
+
+public class Moving {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int roomLength = Integer.parseInt(scan.nextLine());
+        int roomWidth = Integer.parseInt(scan.nextLine());
+        int roomHeight = Integer.parseInt(scan.nextLine());
+
+        int roomVolume = roomLength * roomWidth * roomHeight;
+
+
+
+    }
+}
