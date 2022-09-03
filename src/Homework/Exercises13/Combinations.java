26f288888ec665e89a675518a9e2e89b4dfa0ba9
Bobi <bobi.bg@gmail.com>
2022-09-03 22:05:25 +0300

    Java basics

--- src/Homework/Exercises13/Combinations.java
+++ src/Homework/Exercises13/Combinations.java
@@ -7,6 +7,24 @@ public class Combinations {
 
         Scanner scan = new Scanner(System.in);
 
+        int n = Integer.parseInt(scan.nextLine());
+        int counter = 0;
+
+        for (int x1 = 0; x1 <= n; x1++) {
+            for (int x2 = 0; x2 <= n ; x2++) {
+                for (int x3 = 0; x3 <= n ; x3++) {
+                    if (x1+x2+x3 == n){
+                        counter++;
+                    }
+
+                }
+
+            }
+
+        }
+
+        System.out.println(counter);
+
 
     }
 }
