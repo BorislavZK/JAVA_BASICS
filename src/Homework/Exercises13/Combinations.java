07818d3f9b4383ba27124a9b755e98b11a94e9f9
Bobi <bobi.bg@gmail.com>
2022-09-03 20:58:09 +0300

    Java basics

--- src/Homework/Exercises13/Combinations.java
+++ src/Homework/Exercises13/Combinations.java
@@ -1,7 +1,11 @@
 package Homework.Exercises13;
 
+import java.util.Scanner;
+
 public class Combinations {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
 
     }
 }
