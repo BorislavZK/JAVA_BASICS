7ba9c2e63dc15c59d84b003bb495d4d3bd47bdc9
Bobi <bobi.bg@gmail.com>
2022-09-05 08:49:21 +0300

    Java basics

--- src/Homework/Exercises13/Building.java
+++ src/Homework/Exercises13/Building.java
@@ -12,14 +12,19 @@ public class Building {
 
         for (int i = floors; i >= 1; i--) {
             for (int j = 0; j < rooms ; j++) {
-                if ( i == floors){
-                    System.out.printf("L%d%d",i,j);
+                if (i == floors) {
+                    System.out.printf("L%d%d ", i, j);
 
+                } else if (i % 2 == 0) {
+                    System.out.printf("O%d%d ", i, j);
+                } else {
+                    System.out.printf("A%d%d ", i, j);
+                }
             }
+
+        System.out.println();
         }
 
+
     }
-
-
-}
 }
