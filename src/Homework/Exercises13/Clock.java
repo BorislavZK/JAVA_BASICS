c18b3ac5369e2952629e70454d588cbe46941f32
Bobi <bobi.bg@gmail.com>
2022-09-07 21:39:57 +0300

    Java basics

--- src/Homework/Exercises13/Clock.java
+++ src/Homework/Exercises13/Clock.java
@@ -7,7 +7,7 @@ public class Clock {
         for (int h = 0; h <= 23; h++) {
             for (int m = 0; m <= 59; m++) {
 
-                System.out.printf("%d:%d\n", h,m);
+                System.out.printf("%02d:%02d\n", h,m);
 
             }
 
