4caddde4966ff3ec45d4fa41c17539a08494cb22
Bobi <bobi.bg@gmail.com>
2022-09-08 19:55:51 +0300

    Java basics

---
+++ src/Homework/Exercises14/SumPrimeNonPrime.java
@@ -0,0 +1,12 @@
+package Homework.Exercises14;
+
+import java.util.Scanner;
+
+public class SumPrimeNonPrime {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
