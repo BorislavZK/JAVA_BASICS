b7e5dac6dd9ac87f0143d7bd72ce8863b7411759
Bobi <bobi.bg@gmail.com>
2022-09-08 21:41:38 +0300

    Java basics

---
+++ src/Homework/Exercises14/SpecialNumber.java
@@ -0,0 +1,31 @@
+package Homework.Exercises14;
+
+import java.util.Scanner;
+
+public class SpecialNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                for (int k = 1; k <= 9; k++) {
+                    for (int l = 1; l <= 9; l++) {
+                        boolean specialNumber = n % i == 0 && n % j == 0 && n % k == 0 && n % l == 0;
+
+                        if (specialNumber) {
+
+                            System.out.printf("%d%d%d%d ", i, j, k, l);
+
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+    }
+}
+
--- src/Homework/Exercises14/SumPrimeNonPrime.java
+++ src/Homework/Exercises14/SumPrimeNonPrime.java
@@ -8,5 +8,40 @@ public class SumPrimeNonPrime {
         Scanner scan = new Scanner(System.in);
 
 
+        int primeSum = 0;
+        int nonPrimeSum = 0;
+
+        String input = scan.nextLine();
+
+        while (!input.equals("stop")) {
+
+            int number = Integer.parseInt(input);
+            if (number < 0) {
+                System.out.println("Number is negative.");
+                input = scan.nextLine();
+                continue;
+            }
+
+            boolean flag = true;
+
+            for (int i = 2; i <= number - 1; i++) {
+                if (number % i == 0) {
+                    flag = false;
+                    nonPrimeSum += number;
+                    break;
+                }
+            }
+
+            if (flag) {
+                primeSum += number;
+            }
+
+            input = scan.nextLine();
+        }
+
+        System.out.printf("Sum of all prime numbers is: %d\n", primeSum);
+        System.out.printf("Sum of all non prime numbers is: %d\n", nonPrimeSum);
+
+
     }
 }
