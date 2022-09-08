9523eafe6d90dd803d20b26e9086bcc4b5df8cc5
Bobi <bobi.bg@gmail.com>
2022-09-08 09:51:03 +0300

    Java basics

---
+++ src/Homework/Exercises14/NumberPyramid.java
@@ -0,0 +1,32 @@
+package Homework.Exercises14;
+
+import java.util.Scanner;
+
+public class NumberPyramid {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+        int currentNumber = 0;
+        boolean flag = false;
+
+        for ( int rows =1; rows <=n; rows++){
+            for (int cols =1; cols <= rows; cols++){
+                if (currentNumber > n ){
+                    flag = true;
+                    break;
+                }
+
+                System.out.print(currentNumber + " ");
+                currentNumber++;
+
+            }
+
+            if(flag){
+                break;
+            }
+            System.out.println();
+        }
+    }
+}
