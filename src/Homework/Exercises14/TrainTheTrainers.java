272eb40cb2a7f74d1303e675ed24377443f9867a
Bobi <bobi.bg@gmail.com>
2022-09-09 20:54:20 +0300

    Java basics

--- src/Homework/Exercises14/TrainTheTrainers.java
+++ src/Homework/Exercises14/TrainTheTrainers.java
@@ -10,17 +10,24 @@ public class TrainTheTrainers {
         int jury = Integer.parseInt(scan.nextLine());
         String input = scan.nextLine();
         double totalGrades = 0;
+        int countGrades =0;
 
         while(!input.equals("Finish")){
             String presentationName = input;
             double sumCurrentPresentationGrade = 0;
             for (int i = 1; i <= jury ; i++) {
                 double grade = Double.parseDouble(scan.nextLine());
-                int countGrades++;
-
+                countGrades++;
+                sumCurrentPresentationGrade +=grade;
             }
+             totalGrades += sumCurrentPresentationGrade;
 
+            System.out.printf("%s-%.2f\n", presentationName, sumCurrentPresentationGrade / jury);
+
+            input = scan.nextLine();
         }
+
+        System.out.printf("Student's final assessment is %.2f.", totalGrades / countGrades );
 
 
     }
