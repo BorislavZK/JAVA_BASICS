6160c6b85d459f9e90f555051c03491f76005f61
Bobi <bobi.bg@gmail.com>
2022-09-13 12:48:42 +0300

    Java basics

---
+++ src/TESTS/ForLoop_basic_test.java
@@ -0,0 +1,14 @@
+package TESTS;
+
+public class ForLoop_basic_test {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 20 ; i++) {
+            if (i%3 == 0 || i == 1){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
