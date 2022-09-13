fe268e5e4a0d8e5186a294467c3636d4d9f3ffd6
Bobi <bobi.bg@gmail.com>
2022-09-13 20:57:02 +0300

    Java basics

---
+++ src/Homework/ExamPreparation/SeriesCalculator.java
@@ -0,0 +1,18 @@
+package Homework.ExamPreparation;
+
+import java.util.Scanner;
+
+public class SeriesCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String tvSeries = scan.nextLine();
+        int seasons = Integer.parseInt(scan.nextLine());
+        int episodes = Integer.parseInt(scan.nextLine());
+        int episodeLengthNoAdv = Integer.parseInt(scan.nextLine());
+
+
+
+
+    }
+}
