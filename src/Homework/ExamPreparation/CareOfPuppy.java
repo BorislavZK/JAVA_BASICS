fda12df04f845942aa81624f9903cb3d3b0951cd
Bobi <bobi.bg@gmail.com>
2022-09-15 22:36:42 +0300

    Java basics

---
+++ src/Homework/ExamPreparation/CareOfPuppy.java
@@ -0,0 +1,33 @@
+package Homework.ExamPreparation;
+
+import java.util.Scanner;
+
+public class CareOfPuppy {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int foodInKg = Integer.parseInt(scan.nextLine());
+        int foodInGr = foodInKg * 1000;
+        int allEatenFood = 0;
+        String input = scan.nextLine();
+
+        while (!input.equals("Adopted")){
+            int currentFood = Integer.parseInt(input);
+            allEatenFood += currentFood;
+
+            input = scan.nextLine();
+        }
+
+        int diff = Math.abs(allEatenFood - foodInGr);
+
+        if (allEatenFood >= foodInGr){
+
+            System.out.printf("Food is enough! Leftovers: %d grams.", diff);
+        }else {
+
+            System.out.printf("Food is enough! Leftovers: %d grams.", diff);
+        }
+
+    }
+}
