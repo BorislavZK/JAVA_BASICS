7b2c037df33e6c5804bc4a9c2cbdd45cf1a50fee
Bobi <bobi.bg@gmail.com>
2022-09-16 22:18:21 +0300

    Java basics

--- src/Homework/ExamPreparation/SuitcasesLoad.java
+++ src/Homework/ExamPreparation/SuitcasesLoad.java
@@ -7,7 +7,35 @@ public class SuitcasesLoad {
 
         Scanner scan = new Scanner(System.in);
 
+        double capacity = Double.parseDouble(scan.nextLine());
+        String input = scan.nextLine();
+        boolean freeSpace = true;
+        int counter = 0;
 
+        while (!input.equals("End")) {
+            double suitcase = Double.parseDouble(input);
+            counter++;
+
+
+            if (counter % 3 == 0) {
+                suitcase = suitcase + (suitcase * 0.1);
+            }
+
+            if (capacity < suitcase) {
+                System.out.printf("No more space!%n");
+                freeSpace = false;
+                counter--;
+                break;
+            }
+
+            capacity -= suitcase;
+            input = scan.nextLine();
+        }
+
+        if (freeSpace) {
+            System.out.println("Congratulations! All suitcases are loaded!");
+        }
+        System.out.printf("Statistic: %d suitcases loaded.", counter);
 
 
     }
