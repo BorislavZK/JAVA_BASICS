3476be102169749bb5560fb8b4e80f98089c8ee6
Bobi <bobi.bg@gmail.com>
2022-09-17 20:06:46 +0300

    Java basics

--- src/Homework/ExamPreparation/DivisionWithoutRemainder.java
+++ src/Homework/ExamPreparation/DivisionWithoutRemainder.java
@@ -9,14 +9,33 @@ public class DivisionWithoutRemainder {
         Scanner scan = new Scanner(System.in);
 
         int n = Integer.parseInt(scan.nextLine());
+        double naDve = 0;
+        double naTri = 0;
+        double naChetiri = 0;
 
-        for (int i = 1; i <= n ; i++) {
+
+        for (int i = 1; i <= n; i++) {
 
             int number = Integer.parseInt(scan.nextLine());
 
+            if (number % 2 == 0) {
+                naDve++;
+            }
+
+            if (number % 3 == 0) {
+                naTri++;
+            }
+
+            if (number % 4 == 0) {
+                naChetiri++;
+            }
 
 
         }
+
+        System.out.printf("%.2f %%\n", (naDve / n) * 100);
+        System.out.printf("%.2f %%\n", (naTri / n) * 100);
+        System.out.printf("%.2f %%\n", (naChetiri / n) * 100);
 
     }
 }
