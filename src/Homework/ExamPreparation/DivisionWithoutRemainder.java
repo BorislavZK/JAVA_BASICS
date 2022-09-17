e26e93b4cc9abc2822344a8281662f7f3b8bb414
Bobi <bobi.bg@gmail.com>
2022-09-17 18:57:59 +0300

    Java basics

---
+++ src/Homework/ExamPreparation/DivisionWithoutRemainder.java
@@ -0,0 +1,22 @@
+package Homework.ExamPreparation;
+
+import java.util.Scanner;
+
+public class DivisionWithoutRemainder {
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+        for (int i = 1; i <= n ; i++) {
+
+            int number = Integer.parseInt(scan.nextLine());
+
+
+
+        }
+
+    }
+}
