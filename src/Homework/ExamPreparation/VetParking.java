2e665fd8fb559c3cb28933b0d1a4583743baa426
Bobi <bobi.bg@gmail.com>
2022-09-17 16:49:15 +0300

    Java basics

---
+++ src/Homework/ExamPreparation/VetParking.java
@@ -0,0 +1,15 @@
+package Homework.ExamPreparation;
+
+import java.util.Scanner;
+
+public class VetParking {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int days = Integer.parseInt(scan.nextLine());
+        int hours = Integer.parseInt(scan.nextLine());
+
+
+    }
+}
