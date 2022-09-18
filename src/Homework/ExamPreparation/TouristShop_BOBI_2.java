226178a7f10217e1b4cbc319f03f2de1ee57ec93
Bobi <bobi.bg@gmail.com>
2022-09-18 15:29:03 +0300

    Java basics

---
+++ src/Homework/ExamPreparation/TouristShop_BOBI_2.java
@@ -0,0 +1,12 @@
+package Homework.ExamPreparation;
+
+import java.util.Scanner;
+
+public class TouristShop_BOBI_2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
