a1f5a695567043b3d3842c13ecd8203a2b6be359
Bobi <bobi.bg@gmail.com>
2022-09-18 16:10:51 +0300

    Java basics

---
+++ src/TESTS/ModulTest.java
@@ -0,0 +1,19 @@
+package TESTS;
+
+public class ModulTest {
+    public static void main(String[] args) {
+
+        int i = 2;
+
+        //if (i % 2 == 0 && i != 0) {
+        // i = i + (i * 2);
+        //}
+
+
+        if (i % 2 == 0 && i != 0) {
+            System.out.println("IN");
+        } else {
+            System.out.println("OUT");
+        }
+    }
+}
