8709c785e9f3e8bc869c47265ca40dadc5a44e54
Bobi <bobi.bg@gmail.com>
2022-09-18 15:45:00 +0300

    Java basics

--- src/Homework/ExamPreparation/TouristShop_BOBI_2.java
+++ src/Homework/ExamPreparation/TouristShop_BOBI_2.java
@@ -5,7 +5,34 @@ import java.util.Scanner;
 public class TouristShop_BOBI_2 {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+
+        double budget = Double.parseDouble(scan.nextLine());
+        String product = scan.nextLine();
+
+        int counter = 0;
+        double totalSum = 0;
+
+        while (!product.equals("Stop")) {
+            double price = Double.parseDouble(scan.nextLine());
+            counter++;
+
+            if (counter % 3 == 0) {
+                price *= 0.5;
+            }
+
+            if ( budget < price) {
+                double moneyNeeded = Math.abs( budget - price);
+                System.out.printf("You don't have enough money!%nYou need %.2f leva!", moneyNeeded);
+                return;
+            }
+
+            totalSum += price;
+            budget -= price;
+            product = scan.nextLine();
+        }
+
+        System.out.printf("You bought %d products for %.2f leva.", counter, totalSum);
 
 
     }
