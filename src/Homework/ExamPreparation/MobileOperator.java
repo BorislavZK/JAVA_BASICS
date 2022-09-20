aafd59638095cc5febf4051fad87d343cee37836
Bobi <bobi.bg@gmail.com>
2022-09-20 09:40:08 +0300

    Java basics

--- src/Homework/ExamPreparation/MobileOperator.java
+++ src/Homework/ExamPreparation/MobileOperator.java
@@ -13,11 +13,46 @@ public class MobileOperator {
         String mobileInternet = scan.nextLine();
         int monthsForPayment = Integer.parseInt(scan.nextLine());
 
+        double pricePerMonth = 0;
 
 
+        switch (typeOfContract) {
+            case "Small":
+                if (termOfContract.equals("one")) {
+                    pricePerMonth = 9.98;
+                } else {
+                    pricePerMonth = 8.58;
+                }
+                break;
 
+            case "Middle":
+                if (termOfContract.equals("one")) {
+                    pricePerMonth = 18.99;
+                } else {
+                    pricePerMonth = 17.09;
+                }
+                break;
 
+            case "Large":
+                if (termOfContract.equals("one")) {
+                    pricePerMonth = 25.98;
+                } else {
+                    pricePerMonth = 23.59;
+                }
+                break;
 
+            case "ExtraLarge":
+                if (termOfContract.equals("one")) {
+                    pricePerMonth = 35.99;
+                } else {
+                    pricePerMonth = 31.79;
+                }
+                break;
+
+            default:
+                break;
+
+        }
 
     }
 }
