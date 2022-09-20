043bd6be8890b0596340b7e26c79aed4170e92e8
Bobi <bobi.bg@gmail.com>
2022-09-20 21:17:04 +0300

    Java basics

--- src/Homework/ExamPreparation/MobileOperator.java
+++ src/Homework/ExamPreparation/MobileOperator.java
@@ -12,6 +12,7 @@ public class MobileOperator {
         String typeOfContract = scan.nextLine();
         String mobileInternet = scan.nextLine();
         int monthsForPayment = Integer.parseInt(scan.nextLine());
+        double totalSum = 0;
 
         double pricePerMonth = 0;
 
@@ -54,12 +55,6 @@ public class MobileOperator {
 
         }
 
-        //при добавен мобилен интернет, към таксата за един месец се добавя:
-        //при такса по-малка или равна на 10.00 лв.  5.50 лв.
-        //при такса по-малка или равна на 30.00 лв.  4.35 лв.
-        //при такса по-голяма от 30.00 лв.  3.85 лв.
-        //ако договорът e за две години, общата сума се намалява с 3.75%
-
         if ("yes".equals(mobileInternet)) {
             if (pricePerMonth <= 10) {
                 pricePerMonth += 5.50;
@@ -69,6 +64,16 @@ public class MobileOperator {
                 pricePerMonth += 3.85;
             }
         }
+
+        totalSum = pricePerMonth * monthsForPayment;
+
+        if (termOfContract.equals("two")){
+            totalSum = totalSum * 0.9625;
+        }
+
+        System.out.printf("%.2f lv.",totalSum);
+
+
 
     }
 }
