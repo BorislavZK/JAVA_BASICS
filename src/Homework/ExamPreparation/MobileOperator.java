b691024ae9740d309dd84df243dae9561a5fc13f
Bobi <bobi.bg@gmail.com>
2022-09-20 09:11:51 +0300

    Java basics

---
+++ src/Homework/ExamPreparation/MobileOperator.java
@@ -0,0 +1,23 @@
+package Homework.ExamPreparation;
+
+import java.util.Scanner;
+
+public class MobileOperator {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+
+        String termOfContract = scan.nextLine();
+        String typeOfContract = scan.nextLine();
+        String mobileInternet = scan.nextLine();
+        int monthsForPayment = Integer.parseInt(scan.nextLine());
+
+
+
+
+
+
+
+    }
+}
