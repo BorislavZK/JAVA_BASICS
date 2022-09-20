16dfde3fd0a09a9b154c0b2429dea3affeb3296f
Bobi <bobi.bg@gmail.com>
2022-09-20 21:49:40 +0300

    Java basics

--- src/Homework/ExamPreparation/PoolDay.java
+++ src/Homework/ExamPreparation/PoolDay.java
@@ -13,10 +13,13 @@ public class PoolDay {
         double chairPrice = Double.parseDouble(scan.nextLine());
         double umbrellaPrice = Double.parseDouble(scan.nextLine());
 
+        double totalEntranceFees = people * entranceFee;  // OK
+        double totalUmbrellaNeeds = Math.ceil(people * 0.5);
+        double totalChairsPrice = Math.ceil((people * 0.75)) * chairPrice;
 
-        double totalEntranceFees = people * entranceFee;
-        double totalUmbrellaNeeds = Math.ceil((people * 1.00) / 2);
-        double totalChairsPRice = (people * 0.75) * chairPrice;
+        double totalAll = totalEntranceFees + totalUmbrellaNeeds * umbrellaPrice + totalChairsPrice;
+
+        System.out.printf("%.2f lv.", totalAll);
 
 
 
