c3e45e56ac0b275cda12e341c111a92e2cada0b8
Bobi <bobi.bg@gmail.com>
2022-09-21 20:27:17 +0300

    Java basics

--- src/Homework/CoffeeMachine.java
+++ src/Homework/CoffeeMachine.java
@@ -10,18 +10,61 @@ public class CoffeeMachine {
         String sugar = scan.nextLine();
         int cups = Integer.parseInt(scan.nextLine());
         double price = 0;
-        double totalPRice = 0;
-
-        //Трябва да имате предвид следните отстъпки:
-        //•	При избрана напитка без захар има 35% отстъпка.
-        //•	При избрана напитка "Espresso" и закупени поне 5 броя, има 25% отстъпка.
-        //•	При сума надвишава 15 лева, 20% отстъпка от крайната цена,
-        //Отстъпките се прилагат в реда на тяхното описване.
+        double totalPrice = 0;
 
 
 
 
+        switch (drink) {
+            case "Espresso":
+                if (sugar.equals("Without")) {
+                    price = 0.90;
 
+                } else if (sugar.equals("Normal")) {
+                    price = 1.00;
+
+                } else {
+                    price = 1.20;
+
+                }
+                break;
+            case "Cappuccino":
+                if (sugar.equals("Without")) {
+                    price = 1.00;
+
+                } else if (sugar.equals("Normal")) {
+                    price = 1.20;
+
+                } else {
+                    price = 1.60;
+
+                }
+                break;
+            case "Tea":
+                if (sugar.equals("Without")) {
+                    price = 0.50;
+
+                } else if (sugar.equals("Normal")) {
+                    price = 0.60;
+
+                } else {
+                    price = 0.70;
+
+                }
+                break;
+        }
+
+        if (sugar.equals("Without")){
+            price *= 0.65;
+        }else if (drink.equals("Espresso") && cups >= 5){
+            price *= 0.75;
+        }
+
+        totalPrice = price * cups;
+
+        if (totalPrice > 15){
+            totalPrice *= 0.80;
+        }
 
 
     }
