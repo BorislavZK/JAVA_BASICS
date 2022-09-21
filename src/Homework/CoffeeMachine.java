dba0924a08edbc82ab0ae89e6e065384bfa1bc94
Bobi <bobi.bg@gmail.com>
2022-09-21 10:36:57 +0300

    Java basics

---
+++ src/Homework/CoffeeMachine.java
@@ -0,0 +1,28 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String drink = scan.nextLine();
+        String sugar = scan.nextLine();
+        int cups = Integer.parseInt(scan.nextLine());
+        double price = 0;
+        double totalPRice = 0;
+
+        //Трябва да имате предвид следните отстъпки:
+        //•	При избрана напитка без захар има 35% отстъпка.
+        //•	При избрана напитка "Espresso" и закупени поне 5 броя, има 25% отстъпка.
+        //•	При сума надвишава 15 лева, 20% отстъпка от крайната цена,
+        //Отстъпките се прилагат в реда на тяхното описване.
+
+
+
+
+
+
+
+    }
+}
