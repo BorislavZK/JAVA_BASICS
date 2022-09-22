9fd9d97040c590d36c15d5d9c2af37799cfe8721
Bobi <bobi.bg@gmail.com>
2022-09-22 17:31:28 +0300

    Java basics

---
+++ src/Homework/ExamPreparation/Club.java
@@ -0,0 +1,31 @@
+package Homework.ExamPreparation;
+
+import java.util.Scanner;
+
+public class Club {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        double profitNeeded = Double.parseDouble(scan.nextLine());
+        String drink = scan.nextLine();
+        double totalProfit = 0;
+
+
+
+        while (!drink.equals("Party!")){
+
+            int drinksCount = Integer.parseInt(scan.nextLine());
+
+            double drinkPrice = drink.length();
+
+
+
+
+            drink = scan.nextLine();
+
+        }
+
+
+    }
+}
