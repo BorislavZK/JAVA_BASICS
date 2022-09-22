7bb0308b106d33fd2a0567b8e439035b76d37e11
Bobi <bobi.bg@gmail.com>
2022-09-22 19:18:31 +0300

    Java basics

--- src/Homework/ExamPreparation/Club.java
+++ src/Homework/ExamPreparation/Club.java
@@ -12,20 +12,37 @@ public class Club {
         double totalProfit = 0;
 
 
-
         while (!drink.equals("Party!")){
 
-            int drinksCount = Integer.parseInt(scan.nextLine());
+            int drinksCount = Integer.parseInt(scan.nextLine()); // Брой напитки
+            double drinkPrice = drink.length(); // Цена на единична напитка
+            double orderPrice = drinksCount * drinkPrice; // Цена на поръчката
 
-            double drinkPrice = drink.length();
 
+            if (orderPrice % 2 != 0) {       // OTSTUPKA
+                orderPrice = orderPrice - (orderPrice * 0.25);
+            }
 
+            totalProfit += orderPrice;
 
+            if (totalProfit >= profitNeeded) {
+                System.out.print("Target acquired.\n");
+                break;
+
+            }
 
             drink = scan.nextLine();
 
+            if (drink.equals("Party!")){
+
+                System.out.printf("We need %.2f leva more.\n", Math.abs(profitNeeded - totalProfit));
+
+                break;
+            }
+
         }
 
+        System.out.printf("Club income - %.2f leva.", totalProfit);
 
     }
 }
