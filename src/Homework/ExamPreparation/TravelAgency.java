50ea1e42ad74d172ea866bcf6f221183e33db07a
Bobi <bobi.bg@gmail.com>
2022-09-22 10:03:02 +0300

    Java basics

--- src/Homework/ExamPreparation/TravelAgency.java
+++ src/Homework/ExamPreparation/TravelAgency.java
@@ -12,17 +12,31 @@ public class TravelAgency {
         String discount = scan.nextLine();
         int days = Integer.parseInt(scan.nextLine());
         double pricePerDay = 0;
-        int discountPercent = 0;
+        double totalPrice = 0;
 
-        switch (town) {
-            case "Bansko":
-            case "Borovets":
 
-                if (equipment.equals("withEquipment")) {
-                    pricePerDay = 100;
-                    if (discount.equals("yes")) {
-                        pricePerDay *= 0.9;
-                    }
+        //На конзолата се отпечатва 1 ред:
+        //•	Когато потребителят е въвел всички данни правилно, отпечатваме:
+        //"The price is {цената, форматирана до втория знак}lv! Have a nice time!"
+        //•	Ако потребителят е въвел по-малко от 1 ден за престой, отпечатваме:
+        //"Days must be positive number!"
+        //•	Когато при въвеждането на града или вида на пакета се въведат невалидни данни, отпечатваме: "Invalid input!"
+
+        if (days < 0){
+            System.out.println("Days must be positive number!");
+            return;
+        }
+
+
+            switch (town) {
+                case "Bansko":
+                case "Borovets":
+
+                    if (equipment.equals("withEquipment")) {
+                        pricePerDay = 100;
+                        if (discount.equals("yes")) {
+                            pricePerDay *= 0.9;
+                        }
 
                 } else {
                     pricePerDay = 80;
@@ -50,6 +64,17 @@ public class TravelAgency {
                 }
 
                 break;
+        }
+
+        totalPrice = pricePerDay * days;
+
+
+        if (town.equals("Bansko") || town.equals("Borovets") || town.equals("Varna") || town.equals("Burgas")
+                || equipment.equals("withEquipment") || equipment.equals("withBreakfast")
+                || equipment.equals("noEquipment") || equipment.equals("noBreakfast")) {
+
+            System.out.printf("The price is %.2dlv! Have a nice time!");
+
         }
 
 
