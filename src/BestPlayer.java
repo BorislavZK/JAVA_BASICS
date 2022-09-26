151438b553da8b03b09e7760a9855d14b3d5ebd7
Bobi <bobi.bg@gmail.com>
2022-09-26 22:32:31 +0300

    Java basics

--- src/BestPlayer.java
+++ src/BestPlayer.java
@@ -5,6 +5,45 @@ public class BestPlayer {
 
         Scanner scan = new Scanner(System.in);
 
+        String player = scan.nextLine();
+        int gols = 0;
+        int maxGoals = Integer.MIN_VALUE;
+        String winner = "";
+        boolean flag = false;
+
+        while (!player.equals("END")) {
+            gols = Integer.parseInt(scan.nextLine());
+
+            if (gols > maxGoals) {
+                maxGoals = gols;
+                winner = player;
+
+                if (maxGoals >= 10) {
+                    flag = true;
+                    //System.out.printf("%s is the best player!", winner);
+                    break;
+                }
+
+            }
+
+            player = scan.nextLine();
+        }
+
+        if (flag) {
+            System.out.printf("%s is the best player!\n",winner);
+            System.out.printf("He has scored %d goals and made a hat-trick !!!",maxGoals);
+            return;
+        }
+
+        if (maxGoals >= 3) {
+            System.out.printf("%s is the best player!\n",winner);
+            System.out.printf("He has scored %d goals and made a hat-trick !!!",maxGoals);
+
+        } else {
+            System.out.printf("%s is the best player!\n",winner);
+            System.out.printf("He has scored %d goals.",maxGoals);
+        }
+
 
 
     }
