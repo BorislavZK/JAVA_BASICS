c2cf8e49eb2bd61eb11f7d30c1023f05164f46db
Bobi <bobi.bg@gmail.com>
2022-09-26 23:08:05 +0300

    Java basics

---
+++ src/Homework/ExamPreparation/BarcodeGenerator.java
@@ -0,0 +1,13 @@
+package Homework.ExamPreparation;
+
+import java.util.Scanner;
+
+public class BarcodeGenerator {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+
+
+    }
+}
