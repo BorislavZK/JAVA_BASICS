892ef83906f8f8b1126742a42e855ac1a4d78e81
Bobi <bobi.bg@gmail.com>
2022-09-27 09:02:37 +0300

    Java basics

--- src/Homework/ExamPreparation/BarcodeGenerator.java
+++ src/Homework/ExamPreparation/BarcodeGenerator.java
@@ -7,19 +7,32 @@ public class BarcodeGenerator {
 
         Scanner scan = new Scanner(System.in);
 
-        String first = scan.nextLine();
-        String second = scan.nextLine();
+        int start = Integer.parseInt(scan.next());
+        int end = Integer.parseInt(scan.next());
 
-        int firstNum = Integer.parseInt(first);
-        int secondNum = Integer.parseInt(second);
+        int firstStart = start / 1000;
+        int secondStart = (start / 100) % 10;
+        int thirdStart = (start / 10) % 10;
+        int fourthStart = start % 10;
 
-        for (int i = firstNum; i <= secondNum ; i++) {
+        int firstEnd = end / 1000;
+        int secondEnd = (end / 100) % 10;
+        int thirdEnd = (end / 10) % 10;
+        int fourthEnd = end % 10;
 
+        for (int i = firstStart; i <= firstEnd; i++) {
+            for (int j = secondStart; j <= secondEnd; j++) {
+                for (int k = thirdStart; k <= thirdEnd; k++) {
+                    for (int l = fourthStart; l <= fourthEnd; l++) {
+                        if (i % 2 != 0 && j % 2 != 0 && k % 2 != 0 && l % 2 != 0) {
 
-
+                            System.out.printf("%d%d%d%d ", i, j, k, l);
+                        }
+                    }
+                }
+            }
         }
-
-
-
     }
 }
+
+
